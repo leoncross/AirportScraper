12e92bcdfc99a3c8810b04567c16221dded0138d
Leon Cross <leon.cross@globalrelay.net>
2019-10-30 15:19:41 +0000

    project setup

--- src/flightChecker.js
+++ src/flightChecker.js
@@ -2,20 +2,21 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
 function Flight() {
-  let flightDetails
+  let flightDetails;
   const gatwick = 'https://www.gatwickairport.com/flights/departures-results/?flight=';
 
-  const getUrl = (code) => {
+  const getUrl = code => {
     if (code) {
-      return gatwick
+      return gatwick;
     }
-  }
+    return false;
+  };
 
   const fetchFlightDetails = (url, code, callback) => {
     fetch(url + code)
       .then(res => res.text())
       .then(res => {
-        const $ = cheerio.load(res)
+        const $ = cheerio.load(res);
 
         flightDetails = {
           time: $('#flight-results-listing > tr > td:nth-child(2)').html(),
@@ -23,22 +24,26 @@ function Flight() {
           code: $('#flight-results-listing > tr > td:nth-child(4)').html(),
           status: $('#flight-results-listing > tr > td.doorStop > span').html(),
           terminal: $('#flight-results-listing > tr > td:nth-child(6)').html(),
-          gate: $('#flight-results-listing > tr > td.gateOpen').html().trim()
-        }
-        callback(flightDetails)
-      })
+          gate: $('#flight-results-listing > tr > td.gateOpen').html()
+        };
 
-  }
+        if (flightDetails.gate !== null) {
+          flightDetails.gate = flightDetails.gate.trim();
+        }
+
+        callback(flightDetails);
+      });
+  };
 
   this.getFlight = (code, callback) => {
-    url = getUrl(code)
-    fetchFlightDetails(url, code, callback)
-  }
+    const url = getUrl(code);
+    fetchFlightDetails(url, code, callback);
+  };
 }
 
-data = 'EJU8195'
+module.exports = Flight
 
-flight = new Flight()
-flight.getFlight(data, (flight) => {
-  console.log(flight);
-})
+// const flight = new Flight();
+// flight.getFlight('EJU8195', details => {
+//   console.log(details);
+// });
